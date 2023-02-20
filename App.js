2d50270a03a1296af61a0b9b47639e5e381742cb
limefresh <limefresh5455@gmail.com>
2023-02-20 19:07:50 +0530

    Todays Changes

--- App.js
+++ App.js
@@ -19,55 +19,55 @@ export default function App() {
   const Stack = createNativeStackNavigator();
 
   return (
-    // <SafeAreaProvider>
-    //   <NavigationContainer>
-    //     <Stack.Navigator initialRouteName="SignIn">
-    //       <Stack.Screen
-    //         name="SignIn"
-    //         component={SignIn}
-    //         options={{ headerShown: false }}
-    //       />
-    //       <Stack.Screen
-    //         name="SignUp"
-    //         component={SignUp}
-    //         options={{ headerShown: false, headerBackTitleVisible: false }}
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="SignIn">
+          <Stack.Screen
+            name="SignIn"
+            component={SignIn}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="SignUp"
+            component={SignUp}
+            options={{ headerShown: false, headerBackTitleVisible: false }}
             
-    //       />
-    //       <Stack.Screen
-    //         name="BusinessProfile"
-    //         component={BusinessProfile}
-    //         options={{ headerShown: false }}
-    //       />
-    //       <Stack.Screen
-    //         name="ShipmentProgressStep"
-    //         component={ShipmentProgressStep}
-    //         options={{ headerShown: false }}
-    //       />
-    //       {/* <Stack.Screen
-    //         name="ShipmentInfo"
-    //         component={ShipmentInfo}
-    //         options={{ headerShown: false }}
-    //       /> */}
-    //       <Stack.Screen
-    //         name="ConfirmShipment"
-    //         component={ConfirmShipment}
-    //         options={{ headerShown: false }}
-    //       />
-    //       <Stack.Screen
-    //         name="Submit"
-    //         component={SubmitShipment}
-    //         options={{ headerShown: false }}
-    //       />
-    //       <Stack.Screen
-    //         name="ConfirmEmail"
-    //         component={ConfirmEmail}
-    //         options={{ headerShown: false }}
-    //       />
-    //     </Stack.Navigator>
-    //   </NavigationContainer>
-    // </SafeAreaProvider>
+          />
+          <Stack.Screen
+            name="BusinessProfile"
+            component={BusinessProfile}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="ShipmentProgressStep"
+            component={ShipmentProgressStep}
+            options={{ headerShown: false }}
+          />
+          {/* <Stack.Screen
+            name="ShipmentInfo"
+            component={ShipmentInfo}
+            options={{ headerShown: false }}
+          /> */}
+          <Stack.Screen
+            name="ConfirmShipment"
+            component={ConfirmShipment}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Submit"
+            component={SubmitShipment}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="ConfirmEmail"
+            component={ConfirmEmail}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
 
-<BusinessProfile/>
+//  <BusinessProfile/>
   // <ShipmentProgressStep/>
   //   <ShipmentInfo/>
 
